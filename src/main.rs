9e63b168bbe81f0e084fd564b799a184697d4bfe
ComputerBread <contact.computerbread@gmail.com>
2023-08-10 18:06:24 +0200

    bit of cleaning & better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use actix_cors::Cors;
 use actix_web::{get, post, web::{self, Data}, App, HttpResponse, HttpServer, Responder, middleware::Logger};
 use env_logger::Env;
-use log::info;
 use reqwest::Client;
-use secrecy::ExposeSecret;
-use serde::Deserialize;
+use routes::google_routes::get_the_juice;
 
-use crate::config::AppConfig;
 
 pub mod config;
+pub mod routes;
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
@@ -21,121 +19,6 @@ async fn handle_post(data: web::Json<serde_json::Value>) -> impl Responder {
     HttpResponse::Ok()
 }
 
-#[derive(Deserialize, Debug)]
-struct AuthCode {
-    code: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct AccessToken {
-    access_token: String,
-    /*
-    expires_in: u32,
-    id_token: String,
-    refresh_token: String,
-    scope: String,
-    token_type: String,
-    */
-}
-
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct PageInfo {
-    total_results: u32,
-    results_per_page: u32,
-}
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Channel {
-    title: String,
-}
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct BrandingSettings {
-    channel: Channel,
-}
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Item {
-    id: String,
-    branding_settings: BrandingSettings,
-}
-
-/// Example
-/// ```JSON
-/// {
-///  "kind": "youtube#channelListResponse",
-///  "etag": "whatever_this_is",
-///  "pageInfo": {
-///    "totalResults": 1,
-///    "resultsPerPage": 5
-///  },
-///  "items": [
-///    {
-///      "kind": "youtube#channel",
-///      "etag": "oijefa",
-///      "id": "UCIv6GIlP5uXbiu666bOUobQ",
-///      "brandingSettings": {
-///        "channel": {
-///          "title": "ComputerBread",
-///          "description": "oiajf",
-///          "keywords": "...",
-///          "country": "FR"
-///        },
-///        "image": {
-///          "bannerExternalUrl": "https://yt3.googleusercontent.com/58..."
-///        }
-///      }
-///    }
-///  ]
-///}
-/// ```
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ChannelsInfo {
-    kind: String,
-    page_info: PageInfo,
-    items: Vec<Item>,
-}
-
-#[post("/get_the_juice")]
-async fn get_the_juice(data: web::Json<AuthCode>, client: web::Data<Client>, config: web::Data<AppConfig>) -> impl Responder {
-
-    let redirect_uri= String::from("postmessage");
-    let grant_type = String::from("authorization_code");
-
-    let params = [
-        ("code", &data.code),
-        ("client_id", &config.google.client_id),
-        ("client_secret", &config.google.client_secret.expose_secret()),
-        ("redirect_uri", &redirect_uri),
-        ("grant_type", &grant_type)
-    ];
-
-    let res = client.post(&config.google.oauth2_token_url)
-        .form(&params)
-        .send()
-        .await
-        .unwrap();
-    let token_response = res.json::<AccessToken>().await.unwrap();
-    //let token_response = res.json::<serde_json::Value>().await.unwrap();
-    info!("{:?}", token_response);
-
-    let res = client.get(&config.google.youtube_channel_info_url)
-        .header("Authorization", format!("Bearer {}", token_response.access_token))
-        .send()
-        .await
-        .unwrap();
-    let res = res.json::<ChannelsInfo>().await.unwrap();
-    info!("{:?}", res);
-    if res.page_info.total_results > 0 {
-        info!("Channel title: {:?}", res.items[0].branding_settings.channel.title);
-        info!("Channel ID: {:?}", res.items[0].id);
-    }
-
-    HttpResponse::Ok()
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
---
+++ src/routes/google_routes.rs
@@ -0,0 +1,206 @@
+
+use actix_web::ResponseError;
+use actix_web::http::header::ContentType;
+use actix_web::{web, post, HttpResponse, http::StatusCode};
+use log::{error, info};
+use secrecy::ExposeSecret;
+use serde::Deserialize;
+use reqwest::Client;
+use anyhow::anyhow;
+use anyhow::Context;
+use serde_json::json;
+
+use crate::config::AppConfig;
+
+/// The Authorization code the user received in the frontend after signing
+/// in with Google & giving (or not) consent.
+#[derive(Deserialize, Debug)]
+struct AuthCode {
+    code: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AccessToken {
+    access_token: String,
+    /* useless for my use case
+    expires_in: u32,
+    id_token: String,
+    refresh_token: String,
+    scope: String,
+    token_type: String,
+    */
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct PageInfo {
+    total_results: u32,
+    //results_per_page: u32,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Channel {
+    title: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct BrandingSettings {
+    channel: Channel,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Item {
+    id: String,
+    branding_settings: BrandingSettings,
+}
+
+/// Example
+/// ```JSON
+/// {
+///  "kind": "youtube#channelListResponse",
+///  "etag": "whatever_this_is",
+///  "pageInfo": {
+///    "totalResults": 1,
+///    "resultsPerPage": 5
+///  },
+///  "items": [
+///    {
+///      "kind": "youtube#channel",
+///      "etag": "oijefa",
+///      "id": "UCIv6GIlP5uXbiu666bOUobQ",
+///      "brandingSettings": {
+///        "channel": {
+///          "title": "ComputerBread",
+///          "description": "oiajf",
+///          "keywords": "...",
+///          "country": "FR"
+///        },
+///        "image": {
+///          "bannerExternalUrl": "https://yt3.googleusercontent.com/58..."
+///        }
+///      }
+///    }
+///  ]
+///}
+/// ```
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ChannelsInfo {
+    //kind: String,
+    page_info: PageInfo,
+    items: Vec<Item>,
+}
+
+// ERROR shit ------------------------------------------------------------------
+#[derive(thiserror::Error, Debug)]
+pub enum GError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+
+    #[error("{0}")]
+    UserError(String),
+}
+
+impl ResponseError for GError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            GError::UnexpectedError(_) => {
+                error!("{:?}", self);
+                StatusCode::INTERNAL_SERVER_ERROR
+            },
+            GError::UserError(_) => {
+                StatusCode::BAD_REQUEST
+            }
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        let message = match self {
+            GError::UnexpectedError(_) => "Unexpected error, please try again",
+            GError::UserError(msg) => &msg,
+        };
+        let json_response = json!({
+            "message": message,
+        }).to_string();
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::json())
+            .body(json_response)
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+/// get_access_token
+/// 
+/// Get access token of a google user using the authorization code received after
+/// signing in with google.
+/// This is the [Authorization Code flow](https://developers.google.com/identity/oauth2/web/guides/use-code-model)
+async fn get_access_token(auth_code: &AuthCode, client: &Client, config: &AppConfig) -> Result<AccessToken, GError> {
+
+    let redirect_uri= String::from("postmessage");
+    let grant_type = String::from("authorization_code");
+
+    let params = [
+        ("code", &auth_code.code),
+        ("client_id", &config.google.client_id),
+        ("client_secret", &config.google.client_secret.expose_secret()),
+        ("redirect_uri", &redirect_uri),
+        ("grant_type", &grant_type)
+    ];
+
+    let res = client.post(&config.google.oauth2_token_url)
+        .form(&params)
+        .send()
+        .await
+        .map_err(|_| anyhow!("Unable to get access token"))?;
+
+    if res.status() == 400 {
+        return Err(GError::UserError("Bad request".into()));
+    } else if res.status() != 200 {
+        return Err(GError::UnexpectedError(anyhow!("Google is unhappy, unable to get access token")));
+    }
+
+    // TODO: check if scope is present
+
+    let res = res.json::<AccessToken>().await.context("unable to parse access token")?;
+    Ok(res)
+}
+
+/// Request the Youtube data v3 API for information about youtube channels of a
+/// google user.
+/// The goal here to access the title (name) and ID of 
+async fn get_channels_info(token_response: &AccessToken, client:&Client, config: &AppConfig) -> Result<ChannelsInfo, GError> {
+
+    let res = client.get(&config.google.youtube_channel_info_url)
+        .header("Authorization", format!("Bearer {}", token_response.access_token))
+        .send()
+        .await
+        .context("Unable to get channels information")?;
+
+    // yes I reuse code, with ctrl+c, ctrl+v
+    if res.status() == 400 {
+        return Err(GError::UserError("Bad request".into()));
+    } else if res.status() != 200 {
+        return Err(GError::UnexpectedError(anyhow!("Google is unhappy, unable to get channels info")));
+    }
+
+    Ok(res.json::<ChannelsInfo>().await.context("unable to parse channels info")?)
+}
+
+#[post("/get_the_juice")]
+async fn get_the_juice(data: web::Json<AuthCode>, client: web::Data<Client>, config: web::Data<AppConfig>) -> Result<HttpResponse, GError> {
+
+
+    let token_response = get_access_token(&data, &client, &config).await?;
+    let channels_info = get_channels_info(&token_response, &client, &config).await?;
+
+    if channels_info.page_info.total_results > 0 {
+        info!("Channel title: {:?}", channels_info.items[0].branding_settings.channel.title);
+        info!("Channel ID: {:?}", channels_info.items[0].id);
+    }
+
+    Ok(HttpResponse::Ok().body("ok"))
+}
